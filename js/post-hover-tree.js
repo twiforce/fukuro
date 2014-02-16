20322ed229b934384936d60ffa1447b731412068
Simon Twiforce <twiforce@syn-ch.ru>
2014-02-16 11:24:48 +0400

    post-hover-tree.js: starting to work on advanced post hover, dollscript-like

---
+++ js/post-hover-tree.js
@@ -0,0 +1,37 @@
+/**
+ * post-hover-tree.js
+ *
+ * Post hover tree. Because post-hover.js isn't russian enough.
+ *
+ * Usage:
+ *   $config['additional_javascript'][] = 'js/jquery.min.js';
+ *   $config['additional_javascript'][] = 'js/post-hover-tree.js';
+ *
+ */
+
+$(document).ready(function () {
+    function init_hover_tree() {
+        var id;
+        $("div.body > a, .mentioned > a").hover(function() {
+            if(id = $(this).text().match(/^>>(\d+)$/)) {
+                $("#reply_" + id[1]).clone().addClass("hover").css({'position': 'absolute', 'top': $(this).offset().top+20, 'left': $(this).offset().left+20 }).appendTo("body");
+            } else {
+                return;
+            }
+        }, function() {
+            $(".hover").hover(function() {
+            }, function() {
+                setTimeout(function() {
+                    $(".hover").remove();
+                }, 500);
+            })
+        });
+    }
+
+    init_hover_tree(document);
+
+    // allow to work with auto-reload.js, etc.
+    $(document).bind('new_post', function(e, post) {
+        init_hover_tree(post);
+    });
+})
