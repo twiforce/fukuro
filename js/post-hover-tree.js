59f1c286ee01a00178847858aa7a9e378b632657
Simon Twiforce <twiforce@yandex.ru>
2014-08-09 09:59:14 +0400

    post-hover-tree.js: trying to force .hover keep its original size

--- js/post-hover-tree.js
+++ js/post-hover-tree.js
@@ -17,59 +17,60 @@ $(document).ready(function () {
         var id;
 
         // whatever this doesn't work anyway
-        $("html *").click(function(e) {
-            if ( e.target.className.indexOf("hover") < 0 ) {
+        $("html *").click(function (e) {
+            if (e.target.className.indexOf("hover") < 0) {
                 $(".hover").remove();
             }
         });
 
         function init_hover_tree() {
             $("div.body > a, .mentioned > a").on({
-                mouseenter: function() {
-                    if(id = $(this).text().match(/^>>(\d+)$/)) {
-                        $post = $('div.post#reply_' + id[1]);
-                        if($post.length == 0) {
-                            var url = $(this).attr('href').replace(/#.*$/, '');
+                mouseenter: function () {
+                    id = $(this).text().match(/^>>(\d+)$/);
+                    $post = $('div.post#reply_' + id[1]);
+                    if ($post.length == 0) {
+                        var url = $(this).attr('href').replace(/#.*$/, '');
 
-                            if($.inArray(url, dont_fetch_again) != -1) {
-                                return;
-                            }
-                            dont_fetch_again.push(url);
-
-                            $.ajax({
-                                url: url,
-                                context: document.body,
-                                success: function(data) {
-                                    $(data).find('div.post.reply').each(function() {
-                                        if($('#' + $(this).attr('id[1]')).length == 0)
-                                            $('body').prepend($(this).css('display', 'none').addClass('hidden'));
-                                    });
-
-                                    $post = $('div.post#reply_' + id);
-                                    if($post.length > 0) {
-                                        init_hover_tree();
-                                    }
-                                }
-                            });
-                        } else {
-                            $("#reply_" + id[1]).clone().addClass("hover")
-                                .css({'display': 'inline', 'position': 'absolute', 'top': $(this).offset().top + 20, 'left': $(this).offset().left })
-                                .appendTo("body");
-                            hovering = true;
+                        if ($.inArray(url, dont_fetch_again) != -1) {
+                            return;
                         }
+                        dont_fetch_again.push(url);
+
+                        $.ajax({
+                            url: url,
+                            context: document.body,
+                            success: function (data) {
+                                $(data).find('div.post.reply').each(function () {
+                                    if ($('#' + $(this).attr('id[1]')).length == 0)
+                                        $('body').prepend($(this).css('display', 'none').addClass('hidden'));
+                                });
+
+                                $post = $('div.post#reply_' + id);
+                                if ($post.length > 0) {
+                                    init_hover_tree();
+                                }
+                            }
+                        })
                     } else {
-                        hovering = false;
+                        $("#reply_" + id[1]).clone().addClass("hover")
+                            .css({'display': 'inline', 'position': 'absolute', 'top': $(this).offset().top + 20, 'left': $(this).offset().left })
+                            .appendTo("body");
+                        // This is still bad and I should feel bad.
+                        $("#reply_" + id[1]+ ".hover:not(:last-child)").remove();
+                        if ($("body").width()-$("#reply_" + id[1] + ".hover").last().position().left-$("#reply_" + id[1] + ".hover").last().width() < 100)
+                           $("#reply_" + id[1] + ".hover").css({'left': $("body").width()-$("#reply_" + id[1] + ".hover").last().position().left});
+                        hovering = true;
                     }
                 },
-                mouseleave: function() {
-                    $(".hover").hover(function() {
+                mouseleave: function () {
+                    $(".hover").hover(function () {
                         hovering = true;
                         $("#reply_" + id[1]).trigger(init_hover_tree());
-                    }, function() {
+                    }, function () {
                         hovering = false;
                     });
 
-                    $("html").mousemove(function() {
+                    $("html").mousemove(function () {
                         if (!(hovering) && ($(".hover").is(":visible"))) {
                             setTimeout(function () {
                                 $(".hover").remove();
@@ -78,12 +79,13 @@ $(document).ready(function () {
                     })
                 }
             });
+            ;
         }
 
         init_hover_tree(document);
 
         // allow to work with auto-reload.js, etc.
-        $(document).bind('new_post', function(e, post) {
+        $(document).bind('new_post', function (e, post) {
             init_hover_tree(post);
         });
     }
