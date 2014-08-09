151a7cee20ceed1c8e0cf0807f39c8223e0d333c
Simon Twiforce <twiforce@yandex.ru>
2014-08-09 08:36:26 +0400

    noko50-deleter.js: removes extra posts from "Last 50 posts" page

---
+++ js/noko50-deleter.js
@@ -0,0 +1,24 @@
+/*
+ * noko50-deleter.js
+ *
+ * Hides posts on noko50 pages
+ *
+ * Usage:
+ *   $config['additional_javascript'][] = 'js/jquery.min.js';
+ *   $config['additional_javascript'][] = 'js/settings.js';
+ *   $config['additional_javascript'][] = 'js/noko50-deleter.js';
+ *
+ */
+
+onready(function(){
+    var noko50clear = function() {
+        if (window.location.pathname.split("+").pop() == '50.html')
+            while ($(".post.reply").length > 50)
+                $(".post.reply:first").remove();
+    };
+    noko50clear();
+    // allow to work with auto-reload.js, etc.
+    $(document).bind('new_post', function(e, post) {
+        noko50clear();
+    });
+});
