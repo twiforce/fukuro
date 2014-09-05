c96766a638e3507a8e07a49ed0289c27026a061a
Simon Twiforce <twiforce@yandex.ru>
2014-09-05 07:32:49 +0400

    display previews for user flags

--- js/save-user_flag.js
+++ js/save-user_flag.js
@@ -5,4 +5,10 @@ onready(function(){
 	$('select[name=user_flag]').change(function() {
 		window.localStorage.setItem(flagStorage, $(this).val());
 	});
+
+    // i just dunno where to put this
+    $(".flag-preview").attr("class", "flag-preview flag flag-" + $('select[name="user_flag"]').val());
+    $('select[name="user_flag"]').on('change', function() {
+        $(".flag-preview").attr("class", "flag-preview flag flag-" + this.value);
+    });
 });
