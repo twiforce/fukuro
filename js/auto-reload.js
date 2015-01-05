ec51de9e31450a84fed239fdae58e49d6fced6f0
Wyer <stvorice@gmail.com>
2015-01-06 02:10:34 +0500

    faster (I hope) auto-reload.js

--- js/auto-reload.js
+++ js/auto-reload.js
@@ -15,6 +15,7 @@
  */
 
 $(document).ready(function(){
+
 	if($('div.banner').length == 0)
 		return; // not index
 		
@@ -25,78 +26,125 @@ $(document).ready(function(){
 		settings.updateFrequency = 10;
 	}
 	var poll_accuracy = settings.updateFrequency * 1000;
-    var updateGrowl;
-    var connectionGrowl;
-	
-	var poll = function() {
-		$.ajax({
-			url: document.location,
+
+    //kinda cache these
+    var postctrl = $('form[name=postcontrols]');
+    var $br = $('<br class="clear">');
+
+    //growl notifications related
+    //an attempt to isolate growl and ajax functional
+    function growlAjaxOptions() {
+
+        if (!settings.growlEnabled) {
+            return;
+        }
+        options = {
+            beforeSend: function () {
+                this.updateGrowl = $.growl({
+                    message: _('Обновление...')
+                }, {
+                    delay: 0,
+                    allow_dismiss: false
+                });
+            },
+
+            success: function (evnt, jqXHR, options) {
+                this.updateGrowl.close();
+            },
+
+            error: function (evnt, jqXHR, options, error) {
+                this.connectionGrowl = $.growl({
+                    title: "<b>" + _('Соединение потеряно!') + "</b><br>",
+                    message: _('Не удалось получить новые посты.')
+                }, {
+                    type: "warning"
+                });
+                this.updateGrowl.close();
+            }
+        };
+        return options;
+    }
+
+    var poll = function (options) {
+        $.ajax({
+            url: document.location,
             data: {nocache: Math.random()},
-			beforeSend: function() {
+            beforeSend: function () {
+                if (options)
+                    options.beforeSend();
                 $('#updateThread i').addClass('fa-spin');
-                if (settings.growlEnabled && $(".growl-animated > span").last().html() != "Обновление...") {
-                    updateGrowl = $.growl({
-                        message: _('Обновление...')
-                    }, {
-                        delay: 0,
-                        allow_dismiss: false
-                    });
-                }
             },
-            error: function(xhr, status, error) {
-                if (settings.growlEnabled) {
-                    connectionGrowl = $.growl({
-                        title: "<b>" + _('Соединение потеряно!') + "</b><br>",
-                        message: _('Не удалось получить новые посты.')
-                    }, {
-                        type: "warning"
-                    });
-                }
-                updateGrowl.close();
-            },
-			success: function(data) {
-                if (settings.updateThread && settings.growlEnabled) {
-                    updateGrowl.close();
-                }
-                var postctrl = $('form[name=postcontrols]');
+            success: function (data) {
+                //this is for debugging
+                //var startTime = moment();
 
-				$(data).find('div.post.reply').each(function() {
-					var id = $(this).attr('id');
-					if($('#' + id).length == 0) {
-                        var lastPost = postctrl.find('div.post:not(.hover):not(.post-hover):last');
-                        $(this).insertAfter(lastPost);
-                        lastPost.after('<br class="clear">');
+                //at least there should be an op-post
+                var lastPost = postctrl.find('.post').last();
+                var lastId = lastPost.attr('id');
+                var lastIdNumber = Number(lastId.slice(6));
+                var posts = $(data).find('div.post.reply', 'form[name=postcontrols]');
+                //optimization - quit if there's no new posts
+                if (posts.last().attr('id') != lastId) {
 
+                    posts = posts.filter(function (index, element) {
+                        return ($(element).attr('id').slice(6) > lastIdNumber);
+                    })
+
+                        .each(function (index, element) {
+                            $(element).after($br.clone());
+                        });
                         if (settings.useAnimateCSS) {
-                            $(this).addClass('animated fadeIn');
+                            posts.addClass('animated fadeIn');
                         }
-						$(document).trigger('new_post', this);
-					}
-                    if (settings.useMomentJS) {
-                        now = moment();
-                        momentize(document);
+                    if (posts.length) {
+                        $(document).trigger('new_post', posts);
+                        //TODO: make it more loosely-coupled.
+                        if (settings.useMomentJS) {
+                            now = moment();
+                            momentize(document);
+                        }
+
+                        //Should be last action
+                        lastPost.after(posts);
                     }
-				});
+                    }
+                //var time = moment().diff(startTime);
+                //wonder if that'll prevent some memory leaks
+                data = null;
+                //time in milliseconds
+                //console.log(time);
+                if (options)
+                    options.success();
+            },
+            error: function () {
+                if (options)
+                    options.error();
+            },
+            complete: function () {
                 $('#updateThread i').removeClass('fa-spin');
-                updateGrowl.close();
-			}
-		});
-	};
+            }
+        });
+    };
 
     (settings.simpleNavbar) ? $("#navigation").prepend('<a id=\"updateThread\"><i class="fa fa-refresh fa-lg"></i></a>&nbsp;') :
         (device_type == "mobile") ? $("#navigation").prepend('<a id=\"updateThread\"><i class="fa fa-refresh fa-2x"></i></a>&nbsp;') :
             $("#navigation").prepend('<a id=\"updateThread\"><i class="fa fa-refresh"></i> ' + _('Обновить') + '</a>&nbsp;');
 	
 	function pollNewPosts() {
-        setInterval(poll, poll_accuracy);
-	}
+        setInterval(function () {
+            poll(growlAjaxOptions())
+        }, poll_accuracy);
+    }
 	
 	if(settings.updateThread) {
         pollNewPosts();
 	}
 	
 	$('#updateThread').click(function () {
-        poll();
+        poll(growlAjaxOptions());
+
 	});
+
+
 });
 
