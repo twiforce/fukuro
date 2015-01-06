afa61e199ead56184ff8cbe17d7d830bd0ca89f3
Wyer <stvorice@gmail.com>
2015-01-07 02:29:56 +0500

    Forms rewritten to be loaded with require.js

--- js/AMD-contractor.js
+++ js/AMD-contractor.js
@@ -1,11 +1,11 @@
 //kinda makes sense to move it here since it's the furst AMD file ever
 require.config({
-    baseUrl : 'http://fukurodev.com' + '/js/'});
+    baseUrl : '/js/'});
 
 if (settings.quickReply) {
     require(['lib/interact-1.2.1.min', 'AMD/quick-reply'], function (interact, quickReply) {
 //identifying костыли:
-//yep, that's костыль.	
+//yep, that's костыль. Оставлен оригинальным разработчиком.
         require(['interact'], function (int) {
                 quickReply(int);
             }
@@ -15,6 +15,15 @@ if (settings.quickReply) {
 } else if (settings.inlineForm){
     require(['AMD/inline-form'], function(doForm){
         $(document).ready(doForm);
+    })
+} else if(settings.stickyForm){
+    //another kind of костыли
+    $(document).ready(function(){
+        require(['AMD/sticky-form'], function(){});
+    })
+} else if (settings.bottomForm){
+    $(document).ready(function(){
+        require(['AMD/bottom-form'], function(){});
     })
 }
 
---
+++ js/AMD/sticky-form.js
@@ -0,0 +1,161 @@
+/*
+ * sticky-form.js, async edition
+ *
+ * Sticks the reply form to the bottom right edge of the screen.
+ * This file contains a lot of features and really needs to be cleaned up, I'll do it a bit later.
+ *
+ * depends on
+ *   jquery.min.js,
+ *   jquery.textareaCounter.plugin.js,
+ *   js/jquery.form.js
+ *
+ *
+ */
+
+define(['lib/jquery/jquery.textareaCounter.plugin','lib/jquery/jquery.form'],
+    function(counterPlugin, jqueryForm){
+var clearForm = function () {
+    $('#body, input[name="file"], input[name="file_url"], input[name="embed"]').val('');
+};
+
+var postform = $('form[name="post"]');
+postform.attr('id', 'quick-reply');
+postform.addClass('sticky-form');
+postform.css({
+    "position": 'fixed',
+    "bottom": '0px',
+    "right": '0px',
+    "margin-bottom": '40px',
+    'background': '#AAAAAA',
+    'z-index': '5'
+});
+
+if (settings.simpleNavbar) {
+    var hideButton = '<a id="toggleForm"><i class=\"fa fa-compress fa-lg\"></i></a>';
+    var showButton = '<a id="toggleForm"><i class=\"fa fa-arrows-alt fa-lg\"></i></a>';
+    var upButton = '<a id="formPosition"><i class=\"fa fa-chevron-up fa-lg\"></i></a>';
+    var downButton = '<a id="formPosition"><i class=\"fa fa-chevron-down fa-lg\"></i></a>';
+} else if (device_type == "mobile") {
+    var hideButton = '<a id="toggleForm"><i class=\"fa fa-compress fa-2x\"></i></a>';
+    var showButton = '<a id="toggleForm"><i class=\"fa fa-arrows-alt fa-2x\"></i></a>';
+    var upButton = '<a id="formPosition"><i class=\"fa fa-chevron-up fa-2x\"></i></a>';
+    var downButton = '<a id="formPosition"><i class=\"fa fa-chevron-down fa-2x\"></i></a>';
+} else {
+    var hideButton = '<a id="toggleForm"><i class=\"fa fa-compress\"></i> ' + _(' Спрятать') + '</a>';
+    var showButton = '<a id="toggleForm"><i class=\"fa fa-arrows-alt\"></i> ' + _(' Открыть') + '</a>';
+    var upButton = '<a id="formPosition"><i class=\"fa fa-chevron-up\"></i> ' + _(' Поднять') + '</a>';
+    var downButton = '<a id="formPosition"><i class=\"fa fa-chevron-down\"></i> ' + _(' Опустить') + '</a>';
+}
+
+$("#clearForm").after('&nbsp;', hideButton, '&nbsp;', upButton);
+
+$('#toggleForm').click(function () {
+    var link = $(this);
+    postform.slideToggle("100", function () {
+        if ($(this).is(":visible")) {
+            link.html(hideButton);
+            localStorage.setItem("displayForm", true);
+        } else {
+            link.html(showButton);
+            localStorage.setItem("displayForm", false);
+        }
+    });
+});
+if (localStorage.getItem("displayForm") == 'false') {
+    postform.hide();
+    $('#toggleForm').html(showButton)
+}
+$('#formPosition').toggle(function () {
+    $(this).html(downButton);
+    localStorage.setItem("formPosition", 'up');
+    postform.css({
+        "top": "0px",
+        "bottom": "auto"
+    });
+}, function () {
+    $(this).html(upButton);
+    localStorage.setItem("formPosition", 'down');
+    postform.css({
+        "bottom": "0px",
+        "top": "auto"
+    });
+});
+if (localStorage.getItem("formPosition") == 'up') {
+    $('#formPosition').html(downButton);
+    postform.css({
+        "top": "0px",
+        "bottom": "auto"
+    });
+}
+$('.banner').hide();
+
+if (device_type == "mobile") {
+    postform.css({"margin-bottom": '50px'});
+    $('input[name=password]').remove();
+}
+
+
+if (settings.autoResizeForm) {
+    $('textarea[name="body"]').css({"resize": 'horizontal', "overflow": 'hidden'})
+    .keyup(function () {
+        $(this).height(80); // min-height
+        $(this).height(this.scrollHeight);
+    });
+}
+
+// No refresh with jQuery Form Plugin
+if (settings.noRefresh) {
+    var ajaxErrors = false;
+    postform.ajaxForm({
+        beforeSend: function () {
+            var percentVal = '0%';
+        },
+        uploadProgress: function (event, position, total, percentComplete) {
+            var percentVal = percentComplete + '%';
+            $('input[name=post]').attr("disabled", true)
+            .attr('value', _('Отправка: ') + percentVal);
+        },
+        success: function (response, textStatus, xhr, form) {
+            console.log($('response').contents().find('h1').text());
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            console.log("Lol something bad just happened");
+        },
+        complete: function (xhr, textStatus) {
+            if (!(ajaxErrors)) {
+                clearForm();
+                if (typeof Recaptcha != 'undefined') {
+                    Recaptcha.reload();
+                }
+                if ($('#captchaimg')) {
+                    $('#captchaimg').click();
+                }
+                $('input[name=post]').attr("disabled", false)
+                .attr('value', _('Ответить'));
+            }
+        }
+    });
+
+}
+if (settings.textCountForm) {
+    $('#body').textareaCount({
+        'maxCharacterSize': 4000,
+        'originalStyle': 'originalDisplayInfo',
+        'displayFormat': _('#input символов | #left осталось')
+    });
+    $(".originalDisplayInfo").css({
+        "font-size": '0.7em',
+        "clear": 'both'
+    });
+}
+if (settings.showFormOnCite) {
+    $(window).on('cite', function () {
+        if (localStorage.getItem("displayForm") == 'false') {
+            postform.show();
+            $('#toggleForm').html(hideButton);
+            localStorage.setItem("displayForm", true);
+        }
+        $('textarea').height(this.scrollHeight);
+    });
+}
+});
