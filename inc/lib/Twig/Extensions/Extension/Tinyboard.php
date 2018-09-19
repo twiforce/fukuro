4089b8ef1f1dc41c92d2ecfe067d36598a902738
m4gichat <m4gichat@protonmail.com>
2018-09-20 01:28:10 +0300

    twig update

--- inc/lib/Twig/Extensions/Extension/Tinyboard.php
+++ inc/lib/Twig/Extensions/Extension/Tinyboard.php
@@ -13,9 +13,11 @@ class Twig_Extensions_Extension_Tinyboard extends Twig_Extension
 			new Twig_SimpleFilter('filesize', 'format_bytes'),
 			new Twig_SimpleFilter('truncate', 'twig_truncate_filter'),
 			new Twig_SimpleFilter('truncate_body', 'truncate'),
+			new Twig_SimpleFilter('truncate_filename', 'twig_filename_truncate_filter'),
 			new Twig_SimpleFilter('extension', 'twig_extension_filter'),
 			new Twig_SimpleFilter('sprintf', 'sprintf'),
 			new Twig_SimpleFilter('capcode', 'capcode'),
+			new Twig_SimpleFilter('remove_modifiers', 'remove_modifiers'),
 			new Twig_SimpleFilter('hasPermission', 'twig_hasPermission_filter'),
 			new Twig_SimpleFilter('date', 'twig_date_filter'),
 			new Twig_SimpleFilter('poster_id', 'poster_id'),
@@ -25,7 +27,7 @@ class Twig_Extensions_Extension_Tinyboard extends Twig_Extension
 			new Twig_SimpleFilter('until', 'until'),
 			new Twig_SimpleFilter('push', 'twig_push_filter'),
 			new Twig_SimpleFilter('bidi_cleanup', 'bidi_cleanup'),
-			new Twig_SimpleFilter('addslashes', 'addslashes')
+			new Twig_SimpleFilter('addslashes', 'addslashes'),
 		);
 	}
 	
@@ -42,6 +44,10 @@ class Twig_Extensions_Extension_Tinyboard extends Twig_Extension
 			new Twig_SimpleFunction('timezone', 'twig_timezone_function'),
 			new Twig_SimpleFunction('hiddenInputs', 'hiddenInputs'),
 			new Twig_SimpleFunction('hiddenInputsHash', 'hiddenInputsHash'),
+			new Twig_SimpleFunction('ratio', 'twig_ratio_function'),
+			new Twig_SimpleFunction('secure_link_confirm', 'twig_secure_link_confirm'),
+			new Twig_SimpleFunction('secure_link', 'twig_secure_link'),
+			new Twig_SimpleFunction('link_for', 'link_for')
 		);
 	}
 	
@@ -100,3 +106,30 @@ function twig_truncate_filter($value, $length = 30, $preserve = false, $separato
 	return $value;
 }
 
+function twig_filename_truncate_filter($value, $length = 30, $separator = '…') {
+	if (mb_strlen($value) > $length) {
+		$value = strrev($value);
+		$array = array_reverse(explode(".", $value, 2));
+		$array = array_map("strrev", $array);
+		
+		$filename = &$array[0];
+		$extension = isset($array[1]) ? $array[1] : false;
+
+		$filename = mb_substr($filename, 0, $length - ($extension ? mb_strlen($extension) + 1 : 0)) . $separator;
+
+		return implode(".", $array);
+	}
+	return $value;
+}
+
+function twig_ratio_function($w, $h) {
+	return fraction($w, $h, ':');
+}
+function twig_secure_link_confirm($text, $title, $confirm_message, $href) {
+	global $config;
+
+	return '<a onclick="if (event.which==2) return true;if (confirm(\'' . htmlentities(addslashes($confirm_message)) . '\')) document.location=\'?/' . htmlspecialchars(addslashes($href . '/' . make_secure_link_token($href))) . '\';return false;" title="' . htmlentities($title) . '" href="?/' . $href . '">' . $text . '</a>';
+}
+function twig_secure_link($href) {
+	return $href . '/' . make_secure_link_token($href);
+}
